c471d0d898b1554842d2cc8a92837a0125b1f3d9
Humoyin <baxranoofxumoyun@gmail.com>
2024-03-29 15:00:42 +0500

    nimadfir

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const link=document.querySelector('a')
-console.log(link)
-link.setAttribute('href','https://www.yandex.ru')
-link.innerHTML='go to yandex main page'
-const text=document.querySelector('p')
-console.log(text.getAttribute('class'))
-text.setAttribute('class','sccus')
+// const link=document.querySelector('a')
+// console.log(link)
+// link.setAttribute('href','https://www.yandex.ru')
+// link.innerHTML='go to yandex main page'
+// const text=document.querySelector('p')
+// console.log(text.getAttribute('class'))
+// text.setAttribute('class','sccus')
+// text.setAttribute('style','color:red')
+// const heading=document.querySelector('h1')
+// heading.setAttribute('style','margin;50px')
+// heading.style.margin='30px'
+// heading.style.color='red'
