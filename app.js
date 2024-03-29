f1a1352ea189851a2833265b945f7350d34587ac
Humoyin <baxranoofxumoyun@gmail.com>
2024-03-29 14:03:31 +0500

    yangisi

---
+++ app.js
@@ -0,0 +1,7 @@
+const link=document.querySelector('a')
+console.log(link)
+link.setAttribute('href','https://www.yandex.ru')
+link.innerHTML='go to yandex main page'
+const text=document.querySelector('p')
+console.log(text.getAttribute('class'))
+text.setAttribute('class','sccus')
